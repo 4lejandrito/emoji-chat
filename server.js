ba364f1cbf3b6c516b375485f95eb9f88fbd3ba1
Julien C <julien.castelain@liferay.com>
2017-05-09 10:57:32 +0200

    Initialize socket.io in server and client

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(__dirname + '/index.html');
 });
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
+});
+
+io.on('connection', function (socket) {
+
+  io.emit('connected', {id: socket.id});
+
+  socket.on('disconnect', function () {
+    console.log('a user disconnected');
+  });
+
+  socket.on('chatMessage', function (msg) {
+    console.log('got a chat message', msg);
+  });
+
 });
