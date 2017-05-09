3f61d618638ff13d878cb13d4d450e791a1f2417
Alejandro Tardín <alejandro.tardin@liferay.com>
2017-05-09 11:36:53 +0200

    Serving all the files in the root

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var express = require('express');
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
+
+app.use(express.static(__dirname));
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
